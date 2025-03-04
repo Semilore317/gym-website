7c766b06bcc9ca9939268e2ba981096e1b90d6ef
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-03-04 16:37:04 +0100

    tabs section next...

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,43 @@
 import React from "react"
+import {motion} from "framer-motion"
+import {FadeUp} from "../../utility/animation.js";
 
 const Banner = ({image, title, subtitle, link}) => {
     return <div className="container">
         <div className="grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14 bg-[#f9f9f9]">
             {/*Banner Image Section*/}
             <div>
-                <img src={image} alt="" className="w-[300px] md:max-w-[400px] xl:min-w-[600px] h-full object-cover" />
+                <motion.img initial={{opacity: 0, scale:0.5}}
+                            whileInView={{opacity: 1, scale:1}}
+                            transition={{type: "spring", stiffness: 100, delay: 0.2}}
+                            src={image} alt="" className="w-[300px] md:max-w-[400px] xl:min-w-[600px] h-full object-cover" />
             </div>
             {/*Banner Text Section*/}
             <div className="flex flex-col justify-center text-center md:text-left space-y-4 lg:max-w-[500px]">
-                <p className="text-2xl lg:text-4xl font-bold capitalize font-outfit">{title}</p>
-                <p>{subtitle}</p>
-                <div className="flex justify-center md:justify-start">
-                    <button className="primary-btn flex items-center gap-2 mt-4">
-                        Explore More
-                    </button>
-                </div>
+                <motion.p    variants={FadeUp(0.5)}
+                             initial="hidden"
+                             whileInView={"visible"}
+                             viewport={{once:true}}
+                             className="text-2xl lg:text-4xl font-bold capitalize font-outfit">
+                    {title}
+                </motion.p>
+                <motion.p variants={FadeUp(0.7)}
+                          initial="hidden"
+                          whileInView={"visible"}
+                          viewport={{once:true}}>
+                    {subtitle}
+                </motion.p>
+                <motion.div variants={FadeUp(0.9)}
+                            initial="hidden"
+                            whileInView={"visible"}
+                            viewport={{once:true}}
+                            className="flex justify-center md:justify-start">
+                    <a href={link}>
+                        <button className="primary-btn flex items-center gap-2 mt-4">
+                            Explore More
+                        </button>
+                    </a>
+                </motion.div>
             </div>
         </div>
     </div>
